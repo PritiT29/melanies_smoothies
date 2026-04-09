6baf191994ad548d16b1c57773b1b0a154bab50d
Priti Taparia <160182168+PritiT29@users.noreply.github.com>
2026-04-08 20:22:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,17 @@ cnx=st.connection("snowflake")
 session =cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df=my_dataframe.to_pandas()
+
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5)
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+''
 
-        if not selected_row.empty:
-            search_on = selected_row['SEARCH_ON'].iloc[0]
-            st.write(f'The search value for {fruit_chosen} is {search_on}.')
-        else:
-        # Fallback if no match is found
-            search_on = fruit_chosen
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+
     
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
